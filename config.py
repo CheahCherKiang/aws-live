462781599df098fd6f4e146a087c5985b18009e5
CheahCherKiang <102704508+CheahCherKiang@users.noreply.github.com>
2022-04-04 20:22:02 +0800

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-customhost = "employee.ccuybctlirf7.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "bait3273"
-customdb = "employee"
-custombucket = "cheahcherkiang-employee"
-customregion = "us-east-1"
