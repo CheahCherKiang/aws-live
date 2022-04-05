b1704fba14c4e706cfb65406da78896f77b79e3f
CheahCherKiang <102704508+CheahCherKiang@users.noreply.github.com>
2022-04-05 11:52:21 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-custombucket = "s3-ass2-bucket"
+custombucket = "c4-employees"
 customregion = "us-east-1"
-customhost = "employee.ccuybctlirf7.us-east-1.rds.amazonaws.com"
+customhost = "employees.ccuybctlirf7.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "bait3273"
-customdb = "employee"
+customdb = "employees"
 
-access_key = 'ASIA6HZORFFQLUF4VSUK'
-secret_access_key = 'OMcMxY0zsJKQAhv9X9IzoYSEZu6W8R2LN9J8Jzlj'
-session_token = 'FwoGZXIvYXdzEPP//////////wEaDEfizkMeeOUUZC8FayLRAXYcACDCM0ohSY2859cKMx+5Kv1nQitC3Q4iy+9/8EP/5ue9FNdhZJEdUuyZoEvHRrDHWERcGWTXiTME60pD0Dnpf6f3stTdmpPer0O8xqUjBWp2nObEBPE8nlhK1cAfoEITn6kySU1LscDiOWuNYhCY7RA+i+I8xYCntP4eVC9EZrglczQS0HLqf7dLLZIEIU6zodKvzuC7AYZ/kVkxaCyiFNJSH23lJp0528lOmF8kq5a+bCgi/Oll8B1K7JL3YRsRUvNAc1RDCIF/JjUSH28qKLHQrJIGMi1DUBhkL7mYF6hbYzSFSVxxbfzuzDUXytgvX0EBKv4oVReSUFP37KgERV8GxXk='
+access_key = 'ASIA6HZORFFQBSG6CMHS'
+secret_access_key = 'kRLbCyJKrr0cLrhiMcGvKS9bOqkpbhVliR4/+dhs'
+session_token = 'FwoGZXIvYXdzEP3//////////wEaDJdvFQqIQw9s9PZHVCLRATVDDbIdKKeyMUTk4sLtZnZ+o4bxYM1sIOEDUc2YkWx2Xxh0V7+oKctjM/zlFghO25WQvK85XVg4wdcGxG9npCNhy753q6HcS/bFT3h2J6aU2XiXzJ5gdjxRlafxRf1LmGMmuX+PnJ0ywlra9R4vehQ9I3cZywc9pBytaDKeuGJpTPO7SDTXasQ9fKl54Jxl44ppXsVHK65oDPL+Mi1Xe+OVfZiSLDg1temwiH48r5GDmJZ/2SFGbMBQ941LIMTQX3rADNkLo+PcGX1wzzW7cHfvKKzkrpIGMi1f4D6vZNA1w+gMBbEO8NDXEMDXvP0u7GMY2CEs/+qNwGY2RHrax9mte5NXGTo='
