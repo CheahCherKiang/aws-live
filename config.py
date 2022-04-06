054ac72ddfd0ef6fedb4b9d5525232d95bb71b2a
CheahCherKiang <102704508+CheahCherKiang@users.noreply.github.com>
2022-04-06 15:21:24 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ customuser = "aws_user"
 custompass = "bait3273"
 customdb = "employees"
 
-access_key = 'ASIA6HZORFFQBSG6CMHS'
-secret_access_key = 'kRLbCyJKrr0cLrhiMcGvKS9bOqkpbhVliR4/+dhs'
-session_token = 'FwoGZXIvYXdzEP3//////////wEaDJdvFQqIQw9s9PZHVCLRATVDDbIdKKeyMUTk4sLtZnZ+o4bxYM1sIOEDUc2YkWx2Xxh0V7+oKctjM/zlFghO25WQvK85XVg4wdcGxG9npCNhy753q6HcS/bFT3h2J6aU2XiXzJ5gdjxRlafxRf1LmGMmuX+PnJ0ywlra9R4vehQ9I3cZywc9pBytaDKeuGJpTPO7SDTXasQ9fKl54Jxl44ppXsVHK65oDPL+Mi1Xe+OVfZiSLDg1temwiH48r5GDmJZ/2SFGbMBQ941LIMTQX3rADNkLo+PcGX1wzzW7cHfvKKzkrpIGMi1f4D6vZNA1w+gMBbEO8NDXEMDXvP0u7GMY2CEs/+qNwGY2RHrax9mte5NXGTo='
+access_key = 'ASIA6HZORFFQPNEESYXG'
+secret_access_key = 'try1NsJCEHTCz3RMO/BI9J0qWt0Eo2gbuCKD8S2d'
+session_token = 'FwoGZXIvYXdzEBUaDEgfwqu7uWnMOW3ceCLRAfmnjmzWbr+jzCHPTzybLL+9wdlkVVp2Q/Z27YbLcuMf8NAJ6VgNUk0wFlQmLQzRSMqoxaBwtr7q4zyC/oz36VBn9c3sChyZmtlHfoYF9Gyc1L/Q/MtKvik3ypq+wVomNyyJ0jcjLg2aOMsivTeZjbuBkOVchIgqdFE301lkR1vqIOdbDHkvn7NYhcQoaoEFEq6U4o4XHjA+4+EyHFmYtddb6BIxWdLWkrKIOVhKqnebWRJeJsrrUmOuU+bS55yNpOUpOTKUDeIr5+Yq642okQ+tKKOKtJIGMi0vHim8MZjz67VaFJJIoQmgcu5APxfBf1bcSYCRDM8HC3ht22IlBKSj92FWGRI='
